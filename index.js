14750375a9aa5fa962f0d4ff5503481307f0d1ce
RainbowToad <chad_spilker@hotmail.com>
2022-11-17 12:06:45 -0800

    Trial to generate HTML successful

--- index.js
+++ index.js
@@ -45,14 +45,32 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(data) {
-    fs.writeFile(`README.md`, data, (err) =>
-    err ? console.error(err) : console.log(`README generated`));
+    fs.writeFile(`index.html`, data, (err) =>
+    err ? console.error(err) : console.log(`HTML generated`));
 }
+
+function generateHTML(data) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>This is the managers name: ${data.name} </h1>
+    </body>
+    </html>
+    
+  
+  `;
+  }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((data) => {
-        writeToFile(generateMarkdown(data));
+        writeToFile(generateHTML(data));
     })
 }
 
